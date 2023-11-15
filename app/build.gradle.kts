790bbab7fd7b24852e16cd01d65b7b2d1d506cdd
Yusuf Muratov <78204469+passerby29@users.noreply.github.com>
2023-11-15 22:42:07 +0300

    Revert "Добавлен фрагмент "Отель" в Presentation."

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("androidx.navigation.safeargs")
 }
 
 android {
     namespace = "dev.passerby.emtestproject"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "dev.passerby.emtestproject"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -34,41 +33,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures{
-        viewBinding = true
-    }
 }
 
 dependencies {
 
-    implementation(project(path = ":domain"))
-    implementation(project(path = ":data"))
-
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    //coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    //lifecycle
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    //navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    //maskedEditText
-//    implementation("ru.egslava:MaskedEditText:1.0.5")
-    // flexBox
-    implementation("com.google.android.flexbox:flexbox:3.0.0")
-    //imageCarousel
-    implementation("me.relex:circleindicator:2.1.6")
-    implementation("org.imaginativeworld.whynotimagecarousel:whynotimagecarousel:2.1.0")
-    //glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
